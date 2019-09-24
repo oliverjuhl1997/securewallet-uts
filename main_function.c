38e5fd5a52f75f12039075153dab50e6ac6521d8
b0g01234 <55678297+b0g01234@users.noreply.github.com>
2019-09-24 13:57:27 +1000

    Add files via upload

--- main_function.c
+++ main_function.c
@@ -2,10 +2,14 @@
  The main function of the program
 
 *******************************************************************************/
+#include <stdio.h> 
+#include "headerFiles.h"
+#include "gabriel-functions.c"
 
 int main(void) {
+	printf("Test");
+	print_auth_menu();
 
-	
 
-	return 1;
+	return 0;
 }
