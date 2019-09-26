989e8a4b811c0010f05b63d8d8f09bd883152968
b0g01234 <55678297+b0g01234@users.noreply.github.com>
2019-09-27 00:38:28 +1000

    Add files via upload

--- authentication.c
+++ authentication.c
@@ -1,37 +1,37 @@
-#include <stdio.h>  /* scanf,printf,getchar,fgets,fclose,fwrite,fread,fopen */
-#include <string.h> /* strcpy,strcmp,strlen */
-#include <stdlib.h> /* exit,malloc,free */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "headerFiles.h"
 
-#define MAX_USERNAME_LEN 16
-#define MAX_FILENAME_LEN 20
-#define MAX_PWD_LEN 16
-#define DB_NAME "database.txt"
-
-struct file
+/*******************************************************************************
+ * Author: Alistair Coomber
+ * This function uses a switch statement as a response to user input.
+ * Two choices: 1 -> register, 2 -> login
+ * inputs:
+ * - int choice: The number used in the switch statement
+ * outputs: none
+ * - int user_id: an integer value containing the user_id (index) of the user
+ * struct that has logged in.  User can either register or login - Allistair
+*******************************************************************************/
+void auth_choice(int choice)
 {
-  char filename[MAX_FILENAME_LEN];
-  struct file *next;
-};
+  switch(choice){
+    case 1:
+    login();
 
-typedef struct file file_t;
+    break;
+    
+    case 2:
+    register_user();
+    break;
 
-struct user
-{
-  char username[MAX_USERNAME_LEN + 1];
-  char pwd[MAX_PWD_LEN + 1];
-  file_t *files;
-};
+    case 3:
+    exit(0);
 
-typedef struct user user_t;
-
-void create_user();
-void find_user();
-
-int main(void)
-{
-  find_user();
-  return 0;
-}
+    default:
+    printf("Invalid option!\n");
+  }
+} 
 
 /*******************************************************************************
  * Author: Oliver Windall Juhl
@@ -41,24 +41,40 @@ int main(void)
  * inputs:
  * - char username[]: a string containing the username for the user
  * - char password[]: a string containing the password for the user
+
+ Issues: If registering multiple users, the subroutine overwrites the line 
+ rather than adding the username and password to a new line
 *******************************************************************************/
-void register_user()
+void register_user(void)
 {
   user_t user;
   file_t *head = NULL;
+  
+  printf("Enter usename: ");
+  scanf("%s", user.username);
+  printf("Enter password: ");
+  scanf("%s", user.pwd);
 
-  scanf("%s %s", user.username, user.pwd);
+  printf("%s", user.pwd);
+
   /* VALIDATE PASSWORD AND USERNAME*/
+  printf("%d\n", valid_password(user.pwd));
+
+
   user.files = head;
 
-  FILE *file;
-  file = fopen(DB_NAME, "a");
+  FILE *file = NULL;
+  file = fopen(DB_NAME, "w");
   if (file == NULL)
   {
     printf("Unable to open file at path: %s", DB_NAME);
   }
-  fprintf(file, "%s %s", user.username, user.pwd);
+  
+  fprintf(file, "%s %s", user.username, user.pwd); 
   fclose(file);
+
+  printf("successfully registered user\n\n");
+  
 }
 
 /*******************************************************************************
@@ -68,25 +84,23 @@ void register_user()
  * inputs: none
  * outputs: none
 *******************************************************************************/
-void find_user()
+int find_user(char username[], char pwd[])
 {
+  int valid = 0;
   FILE *file = NULL;
   file = fopen(DB_NAME, "r");
   if (file == NULL)
   {
     printf("Unable to open file at path: %s", DB_NAME);
-    perror("Error");
-    return;
+    return 0;
   }
 
   char temp[512];
-  char username[] = "oliverjuhl";
-  char pwd[] = "pwd";
 
   while (fgets(temp, 512, file) != NULL)
   {
-    char temp_username[MAX_USERNAME_LEN];
-    char temp_pwd[MAX_USERNAME_LEN];
+    char temp_username[MAX_USERNAME_LEN + 1];
+    char temp_pwd[MAX_PWD_LEN + 1];
     char *details_ptr = strtok(temp, " ");
     int counter = 0;
     do
@@ -103,30 +117,117 @@ void find_user()
       details_ptr = strtok(NULL, " ");
       counter++;
     } while ((details_ptr != NULL) && counter < 2);
+
     if (strcmp(temp_username, username) == 0 && strcmp(temp_pwd, pwd) == 0)
     {
-      char* user_info = details_ptr;
+      /*
+      char* user_info = details_ptr; */
       printf("Found -> username: %s, password: %s", temp_username, temp_pwd);
-      /* loads user */
+      printf("Loading user...\n");
+      valid = 1;
+      return valid;
+    } 
+    else
+    {
+      printf("User not found\n");
+      return valid;
     }
   }
   fclose(file);
+  return valid;
 }
 
 /*******************************************************************************
- * Author: Oliver Windall Juhl
- * This adds a file to the current user
- * inputs: none
- * outputs: none
+ * Author: Matty B
+ * This function checks whether the password is valid and returns a boolean flag
+ * inputs: 
+ * - char password[]: A string containing the password
+ * outputs:
+ * - int valid: A boolean variable storing the validity of the password
 *******************************************************************************/
-user_t load_user()
-
-/*******************************************************************************
- * Author: Oliver Windall Juhl
- * This adds a file to the current user
- * inputs: none
- * outputs: none
-*******************************************************************************/
-void add_file()
+int valid_password(char password[])
 {
+  int valid = 0;
+
+    /* CHECKING THE SIZE OF PASSWORD */
+    if ((strlen(password) < 6) || (strlen(password) > 16)) {
+      return valid;
+    } 
+    /* LOOP THROUGH THE PASSWORD */
+  int i;
+  for (i=0; i<strlen(password); i++) {
+
+    /* CHECKING FOR ANY SPECIAL CHARACTERS */
+      if(('a' <= password[i] && password[i] <= 'z') && ('A' <=  password[i] && password[i] <= 'Z') && ('0' <=  password[i] && password[i] <= '9'))
+      {
+        valid = 1;
+        return valid;
+      }
+      else
+      {
+        return valid; 
+      }
+  }
+  return valid;
 }
+
+/*******************************************************************************
+ * Author: Gabriel 
+ * This function prints the initial menu for the user
+ * inputs: none
+ * outputs: none
+*******************************************************************************/
+void print_auth_menu(void)
+{
+  printf("Choose two options: \n");
+  printf("Login - Press 1\n");
+  printf("Register User - Press 2\n");
+  printf("Exit Program - Press 3\n");
+}
+
+/*******************************************************************************
+ * Author: Gabriel 
+ * This function handles the login authentication by obtaining all the login 
+ * information and running the appropiate tasks
+ * inputs: none
+ * outputs: none
+*******************************************************************************/
+void login(void) 
+{
+  int valid = 0;
+  char username[MAX_USERNAME_LEN + 1];
+  char pwd[MAX_PWD_LEN + 1];
+
+  printf("Enter login details! (or press 'b' to go back) \n");
+  do {
+    print_login();
+    printf("Enter username: ");
+    scanf("%s", username);
+    if (strcmp(username, "b") == 0) 
+    {
+      main();
+    }
+    printf("Enter password: ");
+    scanf("%s", pwd);
+    if (strcmp(pwd, "b") == 0) 
+    {
+      main();
+    }
+    valid = find_user(username, pwd);
+  } while (valid == 0);
+
+  printf("User succesfully logged in!\n");
+}
+
+/*******************************************************************************
+ * Author: Gabriel 
+ * This function prints the login user interface for the user
+ * inputs: none
+ * outputs: none
+*******************************************************************************/
+void print_login(void) 
+{
+  printf("Enter username and password followed by a space\n");
+  printf("Or enter 'b' to go back\n");
+}
+
