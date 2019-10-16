c413405bc6e2c88b6139a5446d23d2894d1283b9
oliverjuhl1997 <55677836+oliverjuhl1997@users.noreply.github.com>
2019-10-16 23:53:01 +1100

    Add files via upload

--- main_function.c
+++ main_function.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-	if (strcmp(argv[1], "-a") == 0)
+	if (argv[1] != NULL && strcmp(argv[1], "-a") == 0)
 	{
 		printf("------ Authors ------ \n");
 		printf("Gabriel Bogomolets\n");
@@ -16,34 +16,33 @@ int main(int argc, char *argv[])
 		printf("Clark Kong\n");
 		printf("Alistair Coomber\n");
 		printf("Oliver Juhl\n");
+		return 0;
 	}
-	else if (strcmp(argv[1], "-h") == 0)
+	else if (argv[1] != NULL && strcmp(argv[1], "-h") == 0)
 	{
 		printf("------ Help ------ \n");
 		printf("This is SecureWallet.\n");
 		printf("Store your file encrypted in our database, \n");
 		printf("to ensure security on your filesystem.\n");
 		printf("Simply make a new user and start encrypting your files!\n");
+		return 0;
 	}
-	else
+	int choice;
+	user_t *user;
+	user = malloc(sizeof(user_t));
+	user->files = NULL;
+	while (strlen(user->username) == 0 || strlen(user->pwd) == 0)
 	{
-		int choice;
-		user_t *user;
-		user = malloc(sizeof(user_t));
-		user->files = NULL;
-		while (strlen(user->username) == 0 || strlen(user->pwd) == 0)
-		{
-			printf(" -------- Welcome to Digital Secure Wallet! --------\n");
-			print_auth_menu();
-			scanf("%d", &choice);
-			auth_choice(choice, &user);
-		}
-		/* User has logged in */
-		printf("\n"
-					 "------ Welcome %s! ------ \n",
-					 user->username);
-		printf("\n");
-		options_main(&user);
+		printf(" -------- Welcome to Digital Secure Wallet! --------\n");
+		print_auth_menu();
+		scanf("%d", &choice);
+		auth_choice(choice, &user);
 	}
+	/* User has logged in */
+	printf("\n"
+				 "------ Welcome %s! ------ \n",
+				 user->username);
+	printf("\n");
+	options_main(&user);
 	return 0;
 }
